3eeceaf3f2aee3c5d87d5ab067b0b023800cdcea
Oliver Yang <oliverzyang@tencent.com>
2022-01-25 15:39:04 +0800

    add file func

--- tsh.go
+++ tsh.go
@@ -3,9 +3,29 @@ package tsh
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"os"
 )
 
 func MD5(str string) string {
 	hash := md5.Sum([]byte(str))
 	return hex.EncodeToString(hash[:])
+}
+
+func Exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+func IsDIR(path string) (bool, error) {
+	stat, err := os.Stat(path)
+	if err != nil {
+		return false, err
+	}
+	return stat.IsDir(), err
 }
