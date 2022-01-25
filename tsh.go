c4ce89750ff65dffc99296d2292b212e48fb726e
Oliver Yang <oliverzyang@tencent.com>
2022-01-25 15:51:33 +0800

    add readable/writable

--- tsh.go
+++ tsh.go
@@ -4,6 +4,9 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"os"
+
+	"github.com/yang-zzhong/tsh/errors"
+	"golang.org/x/sys/unix"
 )
 
 func MD5(str string) string {
@@ -11,7 +14,7 @@ func MD5(str string) string {
 	return hex.EncodeToString(hash[:])
 }
 
-func Exists(path string) (bool, error) {
+func Exists(path string) (bool, errors.ChainedError) {
 	_, err := os.Stat(path)
 	if err == nil {
 		return true, nil
@@ -19,13 +22,27 @@ func Exists(path string) (bool, error) {
 	if os.IsNotExist(err) {
 		return false, nil
 	}
-	return false, err
+	return false, errors.New(err)
 }
 
-func IsDIR(path string) (bool, error) {
+func IsDIR(path string) (bool, errors.ChainedError) {
 	stat, err := os.Stat(path)
 	if err != nil {
-		return false, err
+		return false, errors.New(err)
 	}
-	return stat.IsDir(), err
+	return stat.IsDir(), errors.New(err)
+}
+
+func Readable(path string) errors.ChainedError {
+	if err := unix.Access(path, unix.R_OK); err != nil {
+		return errors.New(err)
+	}
+	return nil
+}
+
+func Writable(path string) errors.ChainedError {
+	if err := unix.Access(path, unix.W_OK); err != nil {
+		return errors.New(err)
+	}
+	return nil
 }
